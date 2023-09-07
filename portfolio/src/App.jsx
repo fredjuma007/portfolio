2cf951008079c23fc70a393fa4455dedf7d43fa1
fred <fredjuma8@gmail.com>
2023-09-07 07:35:33 +0300

    setup

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css'
+
+function App() {
+  
+  return (
+    <div className={styles.app}>
+     Hello Me 
+
+    </div>
+         
+  )
+}
+
+export default App
---
+++ portfolio/src/utils.js
@@ -0,0 +1,3 @@
+export const getIimageURL = (image) => {
+    return new URL ('assets/${PATH} , import.meta.url).href')
+}; //to dynamically import images
