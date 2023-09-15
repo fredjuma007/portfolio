ca2f48e0cf4afac1f83965cff4467345c360aaa9
fred <fredjuma8@gmail.com>
2023-09-15 20:44:26 +0300

    updated the hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ export const Hero = () => {
         <a href="mailto:fredjuma8@gmail.com" className={styles.contactBtn}>
             Contact Me</a>
     </div>
-    <img src={getImageUrl('hero/heroImage.png')} alt="hero image of me" className={styles.heroImg} />
+    <img src={getImageUrl('hero/heroImage.jpg')} alt="hero image of me" className={styles.heroImg} />
     <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
   </section>
