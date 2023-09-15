515f52f52fe42af96b477439fd0750a5ab567494
fred <fredjuma8@gmail.com>
2023-09-15 18:30:36 +0300

    hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from '../../utils'
+
+export const Hero = () => {
+  return ( 
+  <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.title}>Hello Friend, I'm Fred</h1>
+        <p className={styles.description}>I'm a web developer with experience in using React and Node js.
+            Reach out if you want to learn more and get your website done
+        </p>
+        <a href="mailto:fredjuma8@gmail.com" className={styles.contactBtn}>
+            Contact Me</a>
+    </div>
+    <img src={getImageUrl('hero/heroImage.png')} alt="hero image of me" className={styles.heroImg} />
+    <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+  </section>
+    );
+}
